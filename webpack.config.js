ce1fc171ff2c3f91860e2cf3a84e0964588c7f46
TedEngineer <tedvu184@gmail.com>
2022-11-12 20:41:18 +1100

    Switching to asset inline

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "./dist"),
-    publicPath: "dist/", // this is the default in webpack5 it will look get the path from dist directory
+    //publicPath: "dist/", // this is the default in webpack5 it will look get the path from dist directory
     // public path can also be a CDN
   },
   mode: "none",
@@ -13,7 +13,7 @@ module.exports = {
     rules: [
       {
         test: /\.(png|jpg|ttf|eot|svg|woff(2)?)(\S+)?$/,
-        type: "asset/resource",
+        type: "asset/inline",
       },
     ],
   },
